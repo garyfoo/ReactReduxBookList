3d71efb62f2c6b662c7762e9670ebce24ffd8158
Gary Foo <gfmr90@gmail.com>
2016-02-23 13:33:27 +0800

    First Commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Garie on 22/2/2016.
+ */
+
+export default function() {
+    return [
+        { title: 'Javascript: The Good Parts', pages: 101 },
+        { title: 'Harry Potter', pages: 39 },
+        { title: 'The Dark Tower', pages: 85 },
+        { title: 'Eloquent Ruby', pages: 1 }
+    ]
+}
